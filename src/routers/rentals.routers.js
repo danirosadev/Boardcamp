5606d4864a4f366e4de1e93d1e139a25e0b45ff8
Dani Rosa <danirosa.dev@gmail.com>
2023-02-12 17:59:04 -0300

    fix: rentals routers

--- src/routers/rentals.routers.js
+++ src/routers/rentals.routers.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { getRentals, postNewRental } from "../controllers/rentals.controllers.js";
+import { deleteRental, endRental, getRentals, postNewRental } from "../controllers/rentals.controllers.js";
 import { rentalSchema } from "../schemas/rentalSchema.js";
 import validateSchema from "../middlewares/validateSchema.js";
 
 const rentalsRouter = Router()
 
 rentalsRouter.get("/rentals", getRentals)
-rentalsRouter.post("/rentals", validateSchema(rentalSchema), postNewRental)
-rentalsRouter.post("/rentals/:id/return")
-rentalsRouter.delete("/rentals/:id")
+rentalsRouter.post("/rentals", postNewRental)
+rentalsRouter.post("/rentals/:id/return", endRental)
+rentalsRouter.delete("/rentals/:id", deleteRental)
 
 export default rentalsRouter
