095c4630f13f0642064e5a58bcbdb90d287f95e1
Dani Rosa <danirosa.dev@gmail.com>
2023-02-12 19:10:10 -0300

    fix: customers crus

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -14,7 +14,7 @@ export async function getCustomerById (req, res){
 
     try {
         const customer = await db.query(`SELECT * FROM customers WHERE id = ${id}`)
-        if (!customer) return res.status(404).send("Cliente não existe.")
+        if (customer.rowCount <= 0) return res.status(404).send("Cliente não existe.")
         res.send(customer.rows[0])
     } catch (error) {
         return res.status(500).send(error.message)
@@ -32,6 +32,9 @@ export async function postNewCustomer (req, res){
     try {
         if (newCustomer.phone.length < 10 || newCustomer.phone.length > 11 || newCustomer.cpf.length != 11) return res.status(400).send("Dados inválidos.")
 
+        const isCpf = await db.query(`SELECT * FROM customers WHERE cpf = '${newCustomer.cpf}'`)
+        if (isCpf.rowCount > 0) return res.sendStatus(409)
+
         await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ('${newCustomer.name}', '${newCustomer.phone}', '${newCustomer.cpf}', '${newCustomer.birthday}')`)
         res.status(201).send("Cliente cadastrado.")
     } catch (error) {
@@ -46,8 +49,11 @@ export async function updateCustomer (req, res){
     try {
         if (body.phone.length < 10 || body.phone.length > 11 || body.cpf.length != 11) return res.status(400).send("Dados inválidos.")
 
+        const isCpf = await db.query(`SELECT * FROM customers WHERE cpf = '${newCustomer.cpf}'`)
+        if (isCpf.rowCount > 0) return res.sendStatus(409)
+        
         await db.query(`UPDATE customers SET name = '${body.name}', phone = '${body.phone}', cpf = '${body.cpf}', birthday = '${body.birthday}' WHERE id = ${id}`)
-        res.status(201).send("Cliente atualizado.")
+        res.status(200).send("Cliente atualizado.")
     } catch (error) {
         return res.status(500).send(error.message)
     }
