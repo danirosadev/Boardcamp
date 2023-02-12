e9547ca45c6d27e4b2541325f809b73f059af293
Dani Rosa <danirosa.dev@gmail.com>
2023-02-12 17:45:52 -0300

    fix: get rentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -1,8 +1,9 @@
 import { db } from "../config/database.connection.js";
+import dayjs from "dayjs";
 
 export async function getRentals (req, res){
     try {
-        const rentalsList = await db.query("SELECT * FROM rentals")
+        const rentalsList = await db.query(`SELECT * FROM rentals JOIN customers ON rentals."customerId" = customers.id AND JOIN games ON rentals."gameId" = games.id`)
         res.send(rentalsList.rows)
     } catch (error) {
         return res.status(500).send(error.message)
@@ -17,7 +18,24 @@ export async function postNewRental (req, res){
     }
 
     try {
-        await db.query(`INSERT INTO rentals ("customerId", "gameId", "daysRented") VALUES ('${newRental.customerId}', '${newRental.gameId}', '${newRental.daysRented}')`)
+        const isCustomer = await db.query(`SELECT * FROM customers WHERE id = ${newRental.customerId}`)
+        if(isCustomer.rows.length === 0){
+            return res.sendStatus(400);
+        }
+
+        const isGame = await db.query(`SELECT * FROM games WHERE id = ${newRental.gameId}`)
+        if(isGame.rows.length === 0 || isGame.rows === null){
+            return res.sendStatus(400);
+        }
+
+        if(newRental.daysRented <= 0){
+            return res.sendStatus(400);
+        }
+
+        const rentDate = dayjs().format('DD/MM/YYYY');
+        const originalPrice = Number(daysRented) * Number(isGame.rows[0].pricePerDay);
+
+        await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ('${newRental.customerId}', '${newRental.gameId}', '${rentDate}', '${newRental.daysRented}' 'null', '${originalPrice}, 'null')`)
         res.status(201).send("Novo aluguel inserido.")
     } catch (error) {
         return res.status(500).send(error.message)
@@ -29,6 +47,9 @@ export async function endRental (req, res){
     const body = req.body
 
     try {
+        const isRental = await db.query(`SELECT * FROM rentals WHERE id = ${id}`)
+        if(isRental.rows.length === 0) return res.sendStatus(404)
+        
         await db.query(`UPDATE rentals SET "returnDate" = date_trunc('day', now()) WHERE id = ${id}`)
         res.status(200).send("Jogo devolvido")
     } catch (error) {
